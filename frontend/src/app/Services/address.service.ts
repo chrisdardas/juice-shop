4f170eacf6f22cc3df74429e08d3aa439c15c0b9
bekouvas <79990762+bekouvas@users.noreply.github.com>
2024-12-18 13:16:06 +0200

    Fixing some violations
    Fixing violations in adress.services.ts, administration.service.specs.ts, administration.service.ts

--- frontend/src/app/Services/address.service.ts
+++ frontend/src/app/Services/address.service.ts
@@ -7,6 +7,20 @@ import { environment } from '../../environments/environment'
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { catchError, map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
+interface Address {
+  id: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  country: string
+}
+
+interface AddressResponse {
+  data: Address[]
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,25 +31,39 @@ export class AddressService {
 
   constructor (private readonly http: HttpClient) { }
 
-  get () {
-    return this.http.get(this.host).pipe(map((response: any) => response.data), catchError((err) => { throw err }))
+  get (): Observable<Address[]> {
+    return this.http.get<AddressResponse>(this.host).pipe(
+      map((response: AddressResponse) => response.data),
+      catchError((err) => { throw err })
+    )
   }
 
-  getById (id) {
+  getById (id: string): Observable<Address> {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    return this.http.get(`${this.host}/${id}`).pipe(map((response: any) => response.data), catchError((err: Error) => { throw err }))
+    return this.http.get<AddressResponse>(`${this.host}/${id}`).pipe(
+      map((response: AddressResponse) => response.data[0]),
+      catchError((err: Error) => { throw err })
+    )
   }
 
-  save (params) {
-    return this.http.post(this.host + '/', params).pipe(map((response: any) => response.data), catchError((err) => { throw err }))
+  save (params: Address): Observable<Address> {
+    return this.http.post<AddressResponse>(this.host + '/', params).pipe(
+      map((response: AddressResponse) => response.data[0]),
+      catchError((err) => { throw err })
+    )
   }
 
-  put (id, params) {
+  put (id: string, params: Address): Observable<Address> {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    return this.http.put(`${this.host}/${id}`, params).pipe(map((response: any) => response.data), catchError((err) => { throw err }))
+    return this.http.put<AddressResponse>(`${this.host}/${id}`, params).pipe(
+      map((response: AddressResponse) => response.data[0]),
+      catchError((err) => { throw err })
+    )
   }
 
-  del (id: number) {
-    return this.http.delete(`${this.host}/${id}`).pipe(map((response: any) => response.data), catchError((err) => { throw err }))
+  del (id: string): Observable<void> {
+    return this.http.delete<void>(`${this.host}/${id}`).pipe(
+      catchError((err) => { throw err })
+    )
   }
 }
--- frontend/src/app/Services/administration.service.ts
+++ frontend/src/app/Services/administration.service.ts
@@ -7,6 +7,14 @@ import { environment } from '../../environments/environment'
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { catchError, map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
+interface User {
+  id: number
+  name: string
+  email: string
+  role: string
+}
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +22,51 @@ import { catchError, map } from 'rxjs/operators'
 export class AdministrationService {
   private readonly hostServer = environment.hostServer
   private readonly host = this.hostServer + '/rest/admin'
-
+  public baseUrl = 'http://localhost:3000'
   constructor (private readonly http: HttpClient) { }
 
   getApplicationVersion () {
     return this.http.get(this.host + '/application-version').pipe(
       map((response: any) => response.version),
       catchError((error: Error) => { throw error })
+    )
+  }
+
+  getData(): Observable<{ id: number, name: string }[]> {
+    return this.http.get<{ id: number, name: string }[]>(`${this.baseUrl}/data`);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/users`).pipe(
+      map((response: User[]) => response),
+      catchError((err) => { throw err })
+    )
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/${id}`).pipe(
+      map((response: User) => response),
+      catchError((err) => { throw err })
+    )
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/users`, user).pipe(
+      map((response: User) => response),
+      catchError((err) => { throw err })
+    )
+  }
+
+  updateUser(id: number, user: User): Observable<User> {
+    return this.http.put<User>(`${this.baseUrl}/users/${id}`, user).pipe(
+      map((response: User) => response),
+      catchError((err) => { throw err })
+    )
+  }
+
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/users/${id}`).pipe(
+      catchError((err) => { throw err })
     )
   }
 }
